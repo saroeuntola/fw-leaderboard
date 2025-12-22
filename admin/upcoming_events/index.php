e7bb08003b4b3cf3e61dc768817a9b41393af53e
tola <tolasweatboy@gmail.com>
2025-12-22 12:23:49 +0700

    update

--- admin/upcoming_events/index.php
+++ admin/upcoming_events/index.php
@@ -1,5 +1,9 @@
 <?php
 ob_start();
+                        ini_set('display_errors', 1);
+                        ini_set('display_startup_errors', 1);
+                        error_reporting(E_ALL);
+
 include "../lib/checkroles.php";
 include '../lib/upcoming_event_lib.php';
 include '../lib/users_lib.php';
